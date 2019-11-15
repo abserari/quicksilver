b34300b30cf6e652cf34cb4745c059b212db6714
abser <abserari@gmail.com>
2019-11-16 01:39:39 +0800

    designpattern

--- designpattern/decorator/decorator.py
+++ designpattern/decorator/decorator.py
@@ -1,6 +1,6 @@
 def use_logging(func):
     def wrapper(*args, **kwargs):
-        logging.warn("%s is running" % func.__name__)
+        print("%s is running" % func.__name__)
         return wrapper(*args, **kwargs)
     return wrapper
 
