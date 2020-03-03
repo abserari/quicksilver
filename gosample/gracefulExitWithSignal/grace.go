215b8f80fbc936c9c025c8b94c930479f8b6b42e
abser <abserari@gmail.com>
2020-03-03 23:30:22 +0800

    update for grace signal

---
+++ gosample/gracefulExitWithSignal/grace.go
@@ -0,0 +1,23 @@
+
+func gracefullyShutdown(fn func()) {
+	c := make(chan os.Signal)
+	// 监听信号
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
+
+	go func() {
+		for s := range c {
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM:
+				fmt.Println("退出:", s)
+				fn()
+				os.Exit(0)
+			case syscall.SIGUSR1:
+				fmt.Println("usr1", s)
+			case syscall.SIGUSR2:
+				fmt.Println("usr2", s)
+			default:
+				fmt.Println("其他信号:", s)
+			}
+		}
+	}()
+}
